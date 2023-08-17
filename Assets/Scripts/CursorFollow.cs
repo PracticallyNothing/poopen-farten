c4c2a5e1a09ca2dd1ea49130b2f1ab3f0d5782f0
PracticallyNothing <practicallynothingatall@gmail.com>
2023-08-17 22:05:38 +0300

    Group player and camera together, add cam offset

--- Assets/Scripts/CursorFollow.cs
+++ Assets/Scripts/CursorFollow.cs
@@ -3,15 +3,19 @@ using UnityEngine;
 public class CursorFollow : MonoBehaviour
 {
     GameObject player;
-    
+    [SerializeField] Vector2 offset;
+
     void Start()
     {
         player = FindObjectOfType<PlayerMovement>().gameObject;
     }
 
     void Update() {
+        Vector3 offsetVec3 = offset;
+        offsetVec3.z = 0;
+
         var playerPos = player.transform.position;
-        var cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        var cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition) + offsetVec3;
         transform.position = playerPos + (cursorPos - playerPos) / 3;
     }
 }
