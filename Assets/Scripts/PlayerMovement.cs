78fc7acfa5a414abb5c4a41021456575f764c21a
PracticallyNothing <practicallynothingatall@gmail.com>
2023-08-19 17:15:03 +0300

    Refactor player movement script

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,10 +6,28 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    // Whether hitting the Spacebar does anything.
     public bool canJump = true;
+
     Rigidbody2D myRigidbody = null;
+
+    // Time of last jump. Used in conjunction with delayBetweenJumpsMs.
     DateTime? lastJump = null;
-    const float maxHorizontalVel = 12;
+
+    /// Time to wait before allowing the player to jump again.
+    /// Used to counteract spam-jumping.
+    [SerializeField] int delayBetweenJumpsMs = 250;
+
+    /// How high the character jumps.
+    [SerializeField] float jumpForce = 40;
+
+    /// The fastest speed the player character may move at.
+    /// Increase for faster overall running.
+    [SerializeField] float maxHorizontalVelocity = 12;
+
+    /// The maximum amount of force that can be applied while accelerating.
+    /// Increase if you want faster acceleration.
+    [SerializeField] float maxMoveForce = 2;
 
     // Start is called before the first frame update
     void Start()
@@ -19,30 +37,25 @@ public class PlayerMovement : MonoBehaviour
 
     void FixedUpdate()
     {
-        var vel = myRigidbody.velocity;
-        // Debug.Log(lastJump == null || DateTime.Now - lastJump > new TimeSpan(0, 0, 2));
-
         if (Input.GetKey(KeyCode.Space) && canJump &&
-            (lastJump == null || DateTime.Now - lastJump > new TimeSpan(0, 0, 0, 0, 250)))
+            (lastJump == null
+             || DateTime.Now - lastJump > new TimeSpan(0, 0, 0, 0, delayBetweenJumpsMs)))
         {
             lastJump = DateTime.Now;
-            myRigidbody.AddForce(
-                new Vector3(0, 40, 0),
-                ForceMode2D.Impulse);
+            myRigidbody.AddForce(new Vector3(0, jumpForce, 0), ForceMode2D.Impulse);
             canJump = false;
         }
-        
-        if (Input.GetKey(KeyCode.A)) {
-            myRigidbody.AddForce(
-                new Vector3(-Math.Clamp(maxHorizontalVel - Math.Abs(vel.x), 0, 2), 0, 0),
-                ForceMode2D.Impulse);
-        }
 
-        if (Input.GetKey(KeyCode.D)) {
-            myRigidbody.AddForce(
-                new Vector3(Math.Clamp(maxHorizontalVel - vel.x, 0, 2), 0, 0),
-                ForceMode2D.Impulse);
+        Vector2 vel = myRigidbody.velocity;
+        float moveForce = Math.Clamp(maxHorizontalVelocity - Math.Abs(vel.x), 0, maxMoveForce);
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            myRigidbody.AddForce(new Vector3(-moveForce, 0, 0), ForceMode2D.Impulse);
         }
-        
+        else if (Input.GetKey(KeyCode.D))
+        {
+            myRigidbody.AddForce(new Vector3(moveForce, 0, 0), ForceMode2D.Impulse);
+        }
     }
 }
