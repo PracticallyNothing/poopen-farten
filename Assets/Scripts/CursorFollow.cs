b456275d4c083e95e5fbeab8dce23949087d397d
PracticallyNothing <practicallynothingatall@gmail.com>
2023-08-19 17:37:34 +0300

    Refactor CursorFollow.cs, speed up player

--- Assets/Scripts/CursorFollow.cs
+++ Assets/Scripts/CursorFollow.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 
+/// An object that has its position set between the Player GameObject and the cursor.
 public class CursorFollow : MonoBehaviour
 {
     GameObject player;
@@ -10,12 +11,16 @@ public class CursorFollow : MonoBehaviour
         player = FindObjectOfType<PlayerMovement>().gameObject;
     }
 
+    [SerializeField]
+    [Range(0, 1)]
+    float distanceFromPlayer = 0.5f;
+
     void Update() {
         Vector3 offsetVec3 = offset;
         offsetVec3.z = 0;
 
         var playerPos = player.transform.position;
         var cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition) + offsetVec3;
-        transform.position = playerPos + (cursorPos - playerPos) / 3;
+        transform.position = playerPos + (cursorPos - playerPos) * distanceFromPlayer;
     }
 }
