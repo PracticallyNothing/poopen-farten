62c9e131b44efa70c7990a33560b3a1e66f92a92
PracticallyNothing <practicallynothingatall@gmail.com>
2024-02-18 18:42:24 +0200

    Add sword in ground, automate Interactible script
    - Draw a sword and add it into the scene as an interactible
    - Make StoryInteractibles automatically:
      - Assign their Tag to "Interactible"
      - Give themselves a BoxCollider2D
      - Make their collider a trigger collider

--- Assets/Scripts/StoryInteractible.cs
+++ Assets/Scripts/StoryInteractible.cs
@@ -1,9 +1,24 @@
 using UnityEngine;
 
+[ExecuteInEditMode]
+[RequireComponent(typeof(BoxCollider2D))]
 public class StoryInteractible : MonoBehaviour
 {
-    [SerializeField] Collider2D triggerCollider;
+    Collider2D triggerCollider;
     [SerializeField] GameObject interactionKeySprite;
+
+    void OnEnable() {
+        triggerCollider = GetComponent<BoxCollider2D>();
+    }
+
+    void OnValidate()
+    {
+        if(triggerCollider == null)
+            triggerCollider = GetComponent<BoxCollider2D>();
+
+        triggerCollider.isTrigger = true;
+        tag = "Interactible";
+    }
 
     // Start is called before the first frame update
     void Start()
